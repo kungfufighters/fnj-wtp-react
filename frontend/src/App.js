f68baad9c10af064a81be6937254f6e8bb75c067
JoeyMucci <jmucci314@gmail.com>
2024-09-29 18:19:56 -0400

    started owner idea submission form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <p><strong>Owner - Idea Submission</strong></p>
+        </nav>
       </header>
+
+      <form>
+        <div className="Idea-vert">
+          <div className="Idea-vert Idea-vert-item">
+            <label className="Idea-label" name="ideaL">Idea Name</label>
+            <input className="Idea-text" name="idea" />
+          </div>
+          <div className="Idea-vert Idea-vert-item">
+            <label className="Idea-label" name="statusL">Current Status</label>
+            <select className="Idea-text" name="status">
+              <option value="pursueNow">Pursue Now</option>
+              <option value="keepOpen">Keep Open</option>
+              <option value="shelve">Shelve</option>
+            </select>
+          </div>
+          <div className="Idea-horz">
+          <p>Image Upload (Optional) </p>
+            <button
+                    aria-label="add image"
+                  >
+                    {' '}
+                    <img
+                      src="https://img.icons8.com/upload.png"
+                      alt="buttonpng"
+                      border="0"
+                    />
+            </button>
+          </div>
+          <div className="Idea-horz">
+          <p>Add a new idea</p>
+            <button
+                    aria-label="add idea"
+                  >
+                    {' '}
+                    <img
+                      src="https://img.icons8.com/plus.png"
+                      alt="buttonpng"
+                      border="0"
+                    />
+            </button>
+          </div>
+          <button className="Idea-button" type="submit">Submit Idea(s)</button>
+        </div>
+      </form>
     </div>
   );
 }
