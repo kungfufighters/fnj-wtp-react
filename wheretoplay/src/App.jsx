2257bfee8960a3d1c3304fb9e10c49e6121cc7b9
Neo Maralit <neomaralit@gmail.com>
2024-10-05 00:09:22 -0400

    Changed to vite

---
+++ wheretoplay/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
