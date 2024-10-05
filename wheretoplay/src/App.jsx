5fa7d8df007d85b39d1cf10f1d7b231822c09794
Neo Maralit <neomaralit@gmail.com>
2024-10-05 00:40:41 -0400

    Added login form and folder structure

--- wheretoplay/src/App.jsx
+++ wheretoplay/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import LoginForm from './components/LoginForm'
 
 function App() {
 
   return (
     <>
       <div>
-
+        <LoginForm/>
       </div>
-      <h1>Vite + React</h1>
     </>
   )
 }
