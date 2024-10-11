395c3455979348ff48a4125c964b694002002c42
JoeyMucci <jmucci314@gmail.com>
2024-10-11 19:09:47 -0400

    scaffold for participant page

---
+++ wheretoplay/app/invite/page.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import '../Idea.css';
+
+import React, { useState } from 'react';
+
+export default function Invite() {
+    const [emails, setEmails] = useState(['']);
+
+    const handleSubmit = () => {
+        // TO DO: Implement Backend call to send emails (liekly with Brevo)
+    };
+
+    function addEmail() {
+        setEmails([...emails, '']);
+    }
+
+    function removeEmail() {
+        const newEmails = [...emails];
+        newEmails.pop();
+        setEmails(newEmails);
+    }
+
+    function EmailBox({ i, curEmail }) {
+        i = parseInt(i, 10);
+        const [email, setEmail] = useState(curEmail);
+
+        const changeEmail = (e: { target: { value: string; }; }) => {
+            setEmail(e.target.value);
+            const newEmails = emails;
+            newEmails[i] = e.target.value;
+            setEmails(newEmails);
+        };
+
+        return (
+            <>
+              <div className="Idea-vert Idea-vert-item">
+                  <label className="Idea-label">Email</label>
+                  <input type="text" className="Idea-text" name="idea" value={email} onChange={changeEmail} />
+              </div>
+            </>
+        );
+    }
+
+    return (
+        <div className="Idea">
+          <header className="Idea-header">
+            <nav>
+              <p><strong>Owner - Invite Collaborators</strong></p>
+            </nav>
+          </header>
+
+          <form onSubmit={handleSubmit}>
+            <div className="Idea-vert">
+              {emails.map((email, i) => (
+                <div key={i}>
+                  <EmailBox i={i} curEmail={emails[i]} />
+                </div>
+              ))}
+              {emails.length <= 10 && <button className="Idea-button Idea-vert-item" onClick={addEmail} type="button">
+                Add Email
+                                      </button>}
+              {emails.length > 1 && <button className="Idea-button Idea-vert-item" onClick={removeEmail} type="button">
+                Remove Email
+                                    </button>}
+              <button className="Idea-button Idea-vert-item" type="submit">Send Invites</button>
+            </div>
+          </form>
+        </div>
+      );
+}
