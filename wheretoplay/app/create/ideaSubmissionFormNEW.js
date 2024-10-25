b474a6f7a3c5c63583f84de6f69b76a9c11e8ee6
Gustavo Pajuelo <gp373@njit.edu>
2024-10-25 18:46:00 -0400

    Added Description field.

--- wheretoplay/app/create/ideaSubmissionFormNEW.js
+++ wheretoplay/app/create/ideaSubmissionFormNEW.js
@@ -3,10 +3,10 @@ import './Idea.css'
 import React, { useState } from "react"
 
 const IdeaSubmissionForm = ({ onSubmit }) => {
-  const [ideas, setIdeas] = useState([["", "", "Pursue Now", null]])
+  const [ideas, setIdeas] = useState([["", "", "", "Pursue Now", null]])
 
   function addIdea(e) {
-    setIdeas([...ideas, ["", "", "Pursue Now", null]])
+    setIdeas([...ideas, ["", "", "", "Pursue Now", null]])
   }
 
   function removeIdea(e) {
@@ -20,10 +20,11 @@ const IdeaSubmissionForm = ({ onSubmit }) => {
     onSubmit(ideas);  // Pass submitted ideas to the parent (App.js)
   };
 
-  function IdeaForm({i, name, seg, stat, source}) {
+  function IdeaForm({i, name, seg, desc, stat, source}) {
     i = parseInt(i)
     const [idea, setIdea] = useState(name)
     const [segment, setSegment] = useState(seg)
+    const [description, setDescription] = useState(desc)
     const [status, setStatus] = useState(stat)
     const [img, setImg] = useState(source)
 
@@ -35,7 +36,7 @@ const IdeaSubmissionForm = ({ onSubmit }) => {
       setIdeas(newIdeas)
     }
 
-    
+
     function changeSegment(e) {
       setSegment(e.target.value)
       let newIdeas = ideas
@@ -44,67 +45,83 @@ const IdeaSubmissionForm = ({ onSubmit }) => {
     }
 
 
-    function changeStatus(e) {
-      setStatus(e.target.value)
+    function changeDescription(e) {
+
+      setDescription(e.target.value)
       let newIdeas = ideas
       newIdeas[i][2] = e.target.value
       setIdeas(newIdeas)
     }
 
+
+    function changeStatus(e) {
+
+      setStatus(e.target.value)
+      let newIdeas = ideas
+      newIdeas[i][3] = e.target.value
+      setIdeas(newIdeas)
+    }
+
+
     function changeImage(e) {
       const file = e.target.files[0]
       setImg(URL.createObjectURL(file))
       let newIdeas = ideas
-      newIdeas[i][3] = URL.createObjectURL(file)
+      newIdeas[i][4] = URL.createObjectURL(file)
       setIdeas(newIdeas)
     }
 
     return (
-      <>
-        <div className="Idea-vert Idea-vert-item">
-            <label className="Idea-label" name="ideaL" >Product/Service</label>
-            <input type="text" className="Idea-text" name="idea" value={idea} onChange={changeIdea}/>
-        </div>
-        <div className="Idea-vert Idea-vert-item">
-            <label className="Idea-label" name="ideaL" >Customer Segment</label>
-            <input type="text" className="Idea-text" name="idea" value={segment} onChange={changeSegment}/>
-        </div>
-        <div className="Idea-vert Idea-vert-item">
-          <label className="Idea-label" name="statusL">Current Status</label>
-          <select className="Idea-text" name="status" value={status} onChange={changeStatus}>
-            <option value="pursueNow">Pursue Now</option>
-            <option value="keepOpen">Keep Open</option>
-            <option value="shelve">Shelve</option>
-          </select>
-        </div>
-        <div className="Idea-vert">
-          <label htmlFor={"files" + i} className="Idea-button Idea-vert-item">Select Image</label>
-          <input id={"files" + i}
-                  className="Hide Idea-vert-item"
-                  type="file"
-                  aria-label="add image"
-                  onChange={changeImage}
+        <>
+            <div className="Idea-vert Idea-vert-item">
+                <label className="Idea-label" htmlFor="idea">Product/Service</label>
+                <input type="text" className="Idea-text" name="idea" id="idea" value={idea} onChange={changeIdea}/>
+            </div>
+            <div className="Idea-vert Idea-vert-item">
+                <label className="Idea-label" htmlFor="segment">Customer Segment</label>
+                <input type="text" className="Idea-text" name="segment" id="segment" value={segment} onChange={changeSegment}/>
+            </div>
+            <div className="Idea-vert Idea-vert-item">
+                <label className="Idea-label" htmlFor="description">Description</label>
+                <textarea className="Idea-text" name="description" id="description" value={description}
+                          onChange={changeDescription}></textarea>
+            </div>
+            <div className="Idea-vert Idea-vert-item">
+                <label className="Idea-label" htmlFor="statusL">Current Status</label>
+                <select className="Idea-text" name="status" value={status} onChange={changeStatus}>
+                    <option value="pursueNow">Pursue Now</option>
+                    <option value="keepOpen">Keep Open</option>
+                    <option value="shelve">Shelve</option>
+                </select>
+            </div>
+            <div className="Idea-vert">
+                <label htmlFor={"files" + i} className="Idea-button Idea-vert-item">Select Image</label>
+                <input id={"files" + i}
+                       className="Hide Idea-vert-item"
+                       type="file"
+                       aria-label="add image"
+                       onChange={changeImage}
                 >
-          </input>
-        </div>
-        {img && <img alt={"idea " + i + " logo"} className="Idea-vert-item Square" src={img} />}
-      </>
+                </input>
+            </div>
+            {img && <img alt={"idea " + i + " logo"} className="Idea-vert-item Square" src={img}/>}
+        </>
     )
   }
 
-  return (
-    <div className="Idea">
-      <header className="Idea-header">
-        <nav>
-          <p><strong>Opportunity Submission</strong></p>
-        </nav>
-      </header>
+    return (
+        <div className="Idea">
+            <header className="Idea-header">
+                <nav>
+                    <p><strong>Opportunity Submission</strong></p>
+                </nav>
+            </header>
 
-      <form onSubmit={handleSubmit}>
-        <div className="Idea-vert">
+            <form onSubmit={handleSubmit}>
+                <div className="Idea-vert">
           {ideas.map((idea, i) => (
             <div key={i}>
-              <IdeaForm i={i} name={ideas[i][0]} seg={ideas[i][1]} stat={ideas[i][2]} source={ideas[i][3]}/>
+              <IdeaForm i={i} name={ideas[i][0]} seg={ideas[i][1]} desc={ideas[i][2]} stat={ideas[i][3]} source={ideas[i][4]}/>
             </div>
           ))}
           {ideas.length <= 10 && <button htmlFor="ideasAdd" className="Idea-button Idea-vert-item" onClick={addIdea} type="button">
