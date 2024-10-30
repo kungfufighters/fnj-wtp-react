221b6e09fa73265f55cf79f31bc0f01ef315ecb4
JoeyMucci <jmucci314@gmail.com>
2024-10-30 18:55:34 -0400

    added curVotes state var

--- wheretoplay/app/voting/voting.tsx
+++ wheretoplay/app/voting/voting.tsx
@@ -30,6 +30,7 @@ interface WebSocketMessage {
 export default function Voting({ ideas }: any) {
   const NUMCATS = 6;
   const TIMERLENGTH = 5;
+  const VOTEOPTIONS = 5;
   const form = useForm({ mode: 'uncontrolled' });
   const [currentIdeaIndex, setCurrentIdeaIndex] = useState(0);
   const [currentOptionIndex, setCurrentOptionIndex] = useState(-1);
@@ -40,6 +41,7 @@ export default function Voting({ ideas }: any) {
   const [modalOpened, modalHandlers] = useDisclosure(false);
   const [currentReasonIndex, setCurrentReasonIndex] = useState(-1);
   const [reasonInput, setReasonInput] = useState('');
+  const [curVotes, setCurVotes] = useState(Array.from({ length: NUMCATS }, () => Array.from({ length: VOTEOPTIONS }, () => 0)));
 
   // Check for mobile device
   const isMobile = useMediaQuery('(max-width: 50em)') ?? false;
@@ -139,7 +141,7 @@ export default function Voting({ ideas }: any) {
     const newReasons = [...reasons];
     newReasons[currentReasonIndex] = reasonInput;
     setReasons(newReasons);
-    setReasonInput('');            // Clear input
+    setReasonInput(''); // Clear input
     modalHandlers.close();
   };
 
@@ -212,22 +214,22 @@ export default function Voting({ ideas }: any) {
           gap="sm"
             >
             <Selection caption="Reason to Buy" index={0} infoM="Based on: Unmet need, Effective solution, and Better than current solutions. [HIGH is GOOD]" />
-            {isVoted[0] && <Graph key="0" graphTitle="" votes={[[1, 1], [2, 2], [3, 3], [4, 2], [5, 1]]} />}
+            {isVoted[0] && <Graph key="0" graphTitle="" votes={[[1, curVotes[0][0]], [2, curVotes[0][1]], [3, curVotes[0][2]], [4, curVotes[0][3]], [5, curVotes[0][4]]]} />}
 
             <Selection caption="Market Volume" index={1} infoM="Based on: Current market size and Expected growth. [HIGH is GOOD]" />
-            {isVoted[1] && <Graph key="1" graphTitle="" votes={[[1, 1], [2, 2], [3, 3], [4, 2], [5, 1]]} />}
+            {isVoted[1] && <Graph key="1" graphTitle="" votes={[[1, curVotes[1][0]], [2, curVotes[1][1]], [3, curVotes[1][2]], [4, curVotes[1][3]], [5, curVotes[1][4]]]} />}
 
             <Selection caption="Economic Viability" index={2} infoM="Based on: Margins (value vs. cost), Customers' ability to pay, and Customer stickiness? [HIGH is GOOD]" />
-            {isVoted[2] && <Graph key="2" graphTitle="" votes={[[1, 1], [2, 2], [3, 3], [4, 2], [5, 1]]} />}
+            {isVoted[2] && <Graph key="2" graphTitle="" votes={[[1, curVotes[2][0]], [2, curVotes[2][1]], [3, curVotes[2][2]], [4, curVotes[2][3]], [5, curVotes[2][4]]]} />}
 
             <Selection caption="Obstacles to Implementation" index={3} infoM="Based on: Product development difficulties and Funding challenges [WANT LOW]" />
-            {isVoted[3] && <Graph key="3" graphTitle="" votes={[[1, 1], [2, 2], [3, 3], [4, 2], [5, 1]]} />}
+            {isVoted[3] && <Graph key="3" graphTitle="" votes={[[1, curVotes[3][0]], [2, curVotes[3][1]], [3, curVotes[3][2]], [4, curVotes[3][3]], [5, curVotes[3][4]]]} />}
 
             <Selection caption="Time To Revenue" index={4} infoM="Based on: Development time, Time between product and market readiness, and Length of sale cycle (e.g. hospitals and schools take a long time) [WANT LOW]" />
-            {isVoted[4] && <Graph key="4" graphTitle="" votes={[[1, 1], [2, 2], [3, 3], [4, 2], [5, 1]]} />}
+            {isVoted[4] && <Graph key="4" graphTitle="" votes={[[1, curVotes[4][0]], [2, curVotes[4][1]], [3, curVotes[4][2]], [4, curVotes[4][3]], [5, curVotes[4][4]]]} />}
 
             <Selection caption="Economic Risks" index={5} infoM="Based on: Competitive threats, 3rd party dependencies, and Barriers to adoption. [WANT LOW]" />
-            {isVoted[5] && <Graph key="5" graphTitle="" votes={[[1, 1], [2, 2], [3, 3], [4, 2], [5, 1]]} />}
+            {isVoted[5] && <Graph key="5" graphTitle="" votes={[[1, curVotes[5][0]], [2, curVotes[5][1]], [3, curVotes[5][2]], [4, curVotes[5][3]], [5, curVotes[5][4]]]} />}
         </Stack>
         </Center>
             <Center>
